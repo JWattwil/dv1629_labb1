30efdc97c201be7042273c551caf1773f2a28575
Adam Svensson <58476156+adamzki99@users.noreply.github.com>
2020-09-04 11:08:45 +0200

    Modification to complete assingment

--- lab1_code_students/fork.c
+++ lab1_code_students/fork.c
@@ -3,16 +3,30 @@
 
 int main(int argc, char **argv)
 {
-    pid_t pid;
+    pid_t pid_0;
+    pid_t pid_1;
     unsigned i;
     unsigned niterations = 1000;
-    pid = fork();
-    if (pid == 0) {
+    pid_0 = fork();
+    if (pid_0 == 0){
         for (i = 0; i < niterations; ++i)
-            printf("A = %d, ", i);
-     } else {
+            printf("A = %d, ", pid_0);
+    } else{
         for (i = 0; i < niterations; ++i)
-            printf("B = %d, ", i);
+            printf("B = %d, ", pid_0);
+    } 
+
+    unsigned n;
+    pid_1 = fork();
+    if (pid_1 == 0){
+        for (i = 0; n < niterations; ++n)
+            printf("A = %d, ", pid_1);
+    } else{
+        for (i = 0; n < niterations; ++n)
+            printf("C = %d, ", pid_1);
     }
+    
+
+
     printf("\n");
 }
