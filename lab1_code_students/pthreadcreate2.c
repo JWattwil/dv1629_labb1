7c1c2e36788b3591973e7522c5c483cf2ceda719
Adam Svensson <58476156+adamzki99@users.noreply.github.com>
2020-09-19 15:45:05 +0200

    Task 9, Done

--- lab1_code_students/pthreadcreate2.c
+++ lab1_code_students/pthreadcreate2.c
@@ -5,23 +5,31 @@
 struct threadArgs {
 	unsigned int id;
 	unsigned int numThreads;
+	int squaredID;
 };
 
 void* child(void* params) {
+	
 	struct threadArgs *args = (struct threadArgs*) params;
 	unsigned int childID = args->id;
 	unsigned int numThreads = args->numThreads;
+	args->squaredID = childID * childID;
 	printf("Greetings from child #%u of %u\n", childID, numThreads);
-	free(args);
+	//free(args);
+	return args;
 }
 
 int main(int argc, char** argv) {
 	pthread_t* children; // dynamic array of child threads
 	struct threadArgs* args; // argument buffer
 	unsigned int numThreads = 0;
+	void *result;
+	unsigned int *resultArray;
 	// get desired # of threads
-	if (argc > 1)
+	if (argc > 1){
 		numThreads = atoi(argv[1]);
+	}
+	resultArray = calloc(numThreads, sizeof(unsigned int));
 	children = malloc(numThreads * sizeof(pthread_t)); // allocate array of handles
 	for (unsigned int id = 0; id < numThreads; id++) {
 		// create threads
@@ -35,8 +43,13 @@ int main(int argc, char** argv) {
 	}
 	printf("I am the parent (main) thread.\n");
 	for (unsigned int id = 0; id < numThreads; id++) {
-		pthread_join(children[id], NULL );
+		pthread_join(children[id], (void**)&args );
+		*(resultArray + id) = args->squaredID;
 	}
 	free(children); // deallocate array
+	printf("----------------------------------------------------------------------\n");
+	for (unsigned int i = 0; i < numThreads; i++){
+		printf("Thread ID: %d, square: %d\n" , i, *(resultArray + i));
+	}
 	return 0;
 }
