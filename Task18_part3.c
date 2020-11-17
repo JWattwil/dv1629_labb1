3efffe5bc313ba3913e3a63b963e3c2518e1092a
Adam Svensson <58476156+adamzki99@users.noreply.github.com>
2020-11-17 15:07:23 +0100

    Name change
    v = part
    
    part 2 will be added soon

--- Task18_part3.c
+++ Task18_part3.c
@@ -3,15 +3,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <semaphore.h>
 
-#define KILO (4)//(1024)
-#define MEGA (4*4)//(1024*1024)
+#define KILO (1024)
+#define MEGA (1024*1024)
 #define MAX_ITEMS (64*MEGA)
 #define swap(v, a, b) {unsigned tmp; tmp=v[a]; v[a]=v[b]; v[b]=tmp;}
 
 static int *v;
 
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+int thread_count = 0;
 
 struct 
 thread_args
@@ -71,53 +72,73 @@ partition(int *v, unsigned low, unsigned high, unsigned pivot_index)
     return high;
 }
 
-void* 
+static void* 
 quick_sort(void *arguments)//(int *v, unsigned low, unsigned high)
 {
 	
 	struct thread_args *args = arguments;
     struct thread_args send_struct;
-    unsigned pivot_index;
+    unsigned pivot_index, low_index, high_index;
 
     pthread_t thread;
-    char low_exist, high_exist;
     int thread_error;
 
     /* no need to sort a vector of zero or one element */
     if (args->low >= args->high)
         return NULL;
 
+    low_index = args->low;
+    high_index = args->high;
     /* select the pivot value */
     pivot_index = (args->low + args->high)/2;
 
-    
+
     /* partition the vector */
     pivot_index = partition(args->dataset, args->low, args->high, pivot_index);
 
     
     /*new thread handels the lower portion*/
     send_struct.dataset = args->dataset;
-    send_struct.low = args->low;
-    send_struct.high = pivot_index-1;    
+    send_struct.low = low_index;
+    send_struct.high = pivot_index - 1;    
     
     thread_error = pthread_create(&thread, NULL, quick_sort, &send_struct);                            
     if (thread_error){                          
-        printf("ERROR; return code from pthread_create(thread) is %d\n", thread_error);                            
+        printf("ERROR; return code from pthread_create(thread) is %d. Thread count: %d\n", thread_error, thread_count);                            
         exit(-1);                          
     }
+    thread_count++;
 
     /*current thread handles the upper portion*/
+    args->high = high_index;
     args->low = pivot_index + 1;
     quick_sort(args);
 
     /*wait for the new thread to be finished before going up a level*/
     thread_error = pthread_join(thread, NULL);                            
         if (thread_error){                          
-            printf("ERROR; return code from pthread_join(thread) is %d\n", thread_error);                            
+            printf("ERROR; return code from pthread_join(thread) is %d. Thread count: %d\n", thread_error, thread_count);                            
             exit(-1);                          
         }
 
     return NULL;
+}
+
+void 
+check()
+{
+    int temp = 0;
+    for (unsigned i = 0; i < MAX_ITEMS; i++)
+    {
+        if (v[i] < temp)
+        {
+            printf("Something went wrong!\n");
+            return;
+        }
+        temp = v[i];
+    }
+    printf("No faults!\n");
+    return;
 }
 
 int
@@ -126,14 +147,17 @@ main(int argc, char **argv)
     init_array();
     //print_array();
 	
+
     struct thread_args sendStruct;
-	void *send_ptr;
     sendStruct.dataset = v;
 	sendStruct.low = 0;
 	sendStruct.high = MAX_ITEMS-1;
-	send_ptr = &sendStruct;
+
+    void *send_ptr = &sendStruct;
+
     quick_sort(send_ptr);
 
-    print_array();
+    check();
+    //print_array();
     return 0;
 }
