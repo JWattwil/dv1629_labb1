e778e55f689e278533b5e7e8d66b884581644668
Adam Svensson <58476156+adamzki99@users.noreply.github.com>
2020-11-18 14:40:06 +0100

    Update Task13.c
    Now it does what it is supposed to do. I have implmented an arbitrator to hand out the chopsticks to the professors.

--- Task13.c
+++ Task13.c
@@ -7,75 +7,77 @@
 const char *names[5];
 char chopsticks[5][5];
 
-int getLeft(int id){
-    switch (id)
-    {
-    case 0:
-        return 4;
-    case 1:
-        return 0;
-    case 2:
-        return 1;
-    case 3:
-        return 2;
-    case 4:
-        return 3;
-    default:
-        return -1;
-    }
-}
-int getRight(int id){
-    switch (id)
-    {
-    case 0:
-        return 1;
-    case 1:
-        return 2;
-    case 2:
-        return 3;
-    case 3:
-        return 4;
-    case 4:
-        return 0;
-    default:
-        return -1;
-    }
-}
+pthread_mutex_t arbirator;
 
-void *action (void *arg){
+int 
+getLeft(int id){
+    switch (id)
+    {
+    case 0:
+        return 4;
+    case 1:
+        return 0;
+    case 2:
+        return 1;
+    case 3:
+        return 2;
+    case 4:
+        return 3;
+    default:
+        return -1;
+    }
+}
+int 
+getRight(int id){
+    switch (id)
+    {
+    case 0:
+        return 1;
+    case 1:
+        return 2;
+    case 2:
+        return 3;
+    case 3:
+        return 4;
+    case 4:
+        return 0;
+    default:
+        return -1;
+    }
+}
+void 
+*action (void *arg){
     unsigned long int id = (unsigned long int) arg;
     
     while(1) {
-        sleep(rand() %10 + 2);  //Thinking
-        printf("%s goes for the left chopstick...\n", names[id]);
-        if(chopsticks[id][getLeft(id)] == 0 && chopsticks[getLeft(id)][id] == 0){
+        sleep(rand() %10 + 2); //Thinking
+        if((chopsticks[id][getLeft(id)] == 0 && chopsticks[getLeft(id)][id] == 0) && (chopsticks[id][getRight(id)] == 0 && chopsticks[getRight(id)][id] == 0)){
+            pthread_mutex_unlock(&arbirator);
+
             chopsticks[id][getLeft(id)] = names[id][2];
             chopsticks[getLeft(id)][id] = names[id][2];
-            printf("%s is thinking -> Got left chopstick\n", names[id]);
-            sleep(rand() %3 + 1);   //Thinking
-        }
-        if(chopsticks[id][getLeft(id)] == names[id][2] && chopsticks[getLeft(id)][id] == names[id][2]){
-            printf("%s goes for the right chopstick...\n", names[id]);
-            if(chopsticks[id][getRight(id)] == 0 && chopsticks[getRight(id)][id] == 0){
-                chopsticks[id][getRight(id)] = names[id][2];
-                chopsticks[getRight(id)][id] = names[id][2];
-                printf("%s is eating -> Has both chopsticks\n", names[id]);
-                sleep(rand() %20 + 10);     //Eating
-                // Dropp right chopstick
-                chopsticks[id][getRight(id)] = 0;
-                chopsticks[getRight(id)][id] = 0;
-                printf("%s dropped the right chopstick\n", names[id]);
+            chopsticks[id][getRight(id)] = names[id][2];
+            chopsticks[getRight(id)][id] = names[id][2];
+            printf("%s has both chopsticks\n", names[id]);
 
-                // Dropp left chopstick
-                chopsticks[id][getLeft(id)] = 0;
-                chopsticks[getLeft(id)][id] = 0;
-                printf("%s dropped the left chopstick\n", names[id]);
-            }
-        }
+            pthread_mutex_unlock(&arbirator);
+
+            printf("%s starts eating\n", names[id]);
+            sleep(rand() %20 + 10); //Eating
+
+            // Dropp right chopstick
+            chopsticks[id][getRight(id)] = 0;
+            chopsticks[getRight(id)][id] = 0;
+            // Dropp left chopstick
+            chopsticks[id][getLeft(id)] = 0;
+            chopsticks[getLeft(id)][id] = 0;
+            printf("%s dropped the chopsticks\n", names[id]);
+        }        
     }  
 }
 
-int main()
+int 
+main()
 {
     printf("Seating professors...\n");
     unsigned int nProf = 5;
@@ -83,6 +85,11 @@ int main()
     pthread_t *professors;
     professors = malloc( nProf * sizeof(pthread_t) );
     
+    if (pthread_mutex_init(&arbirator, NULL) != 0)
+    {
+        printf("\n ERROR: mutex init failed\n");
+        exit(-1);
+    }
 
     names[0] = "Tanenbaum";
     names[1] = "Bos";
